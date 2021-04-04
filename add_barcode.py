c777140f47489c16a2e84f2f9a7a610181320048
Ian Goh <ian@iangohy.com>
2021-04-04 14:56:30 +0800

    Added test python code for tof sensor

--- add_barcode.py
+++ add_barcode.py
@@ -1,7 +1,7 @@
 import csv
 
 while True:
-    with open('data.csv', 'a') as data:
+    with open('data.csv', 'a', newline='') as data:
         barcode = input("Enter barcode (enter q to quit): ")
         if barcode.lower() == "q":
             print("Thank you for your hard work!")
@@ -12,5 +12,6 @@ while True:
         while classification != "waste" and classification != "paper" and classification != "plastic" and classification != "can":
             classification = input("Enter category (waste/paper/plastic/can): ")
         writer = csv.writer(data)
+        print("Writing data...")
         writer.writerow([barcode, description, classification])
 
