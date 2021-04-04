8c9607f9d7b993e0501807418807c7e20871c478
Ian Goh <ian@iangohy.com>
2021-04-04 19:34:06 +0800

    Added try except blocks

--- recyclace_thread.py
+++ recyclace_thread.py
@@ -9,7 +9,14 @@ hid = { 4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g', 11: 'h', 12: 'i
 hid2 = { 4: 'A', 5: 'B', 6: 'C', 7: 'D', 8: 'E', 9: 'F', 10: 'G', 11: 'H', 12: 'I', 13: 'J', 14: 'K', 15: 'L', 16: 'M', 17: 'N', 18: 'O', 19: 'P', 20: 'Q', 21: 'R', 22: 'S', 23: 'T', 24: 'U', 25: 'V', 26: 'W', 27: 'X', 28: 'Y', 29: 'Z', 30: '!', 31: '@', 32: '#', 33: '$', 34: '%', 35: '^', 36: '&', 37: '*', 38: '(', 39: ')', 44: ' ', 45: '_', 46: '+', 47: '{', 48: '}', 49: '|', 51: ':' , 52: '"', 53: '~', 54: '<', 55: '>', 56: '?'  }
 
 def readBarcode():
-    fp = open('/dev/hidraw0', 'rb')
+    try:
+        fp = open('/dev/hidraw0', 'rb')
+    except FileNotFoundError:
+        print("Unable to connect to barcode scanner! Is it plugged in?")
+        quit()
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
+        raise
     res = ""
     while True:
         # Read barcode scan
@@ -76,10 +83,14 @@ if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
     logging.basicConfig(format=format, level=logging.INFO,
                         datefmt="%H:%M:%S")
-    setup()
-    
-    barcodeThread = threading.Thread(target=getBarcodeData)
-    barcodeThread.start()
-    white_led_thread = LedThread(21, 15)
-    white_led_thread.start()
-    logging.info("Recyclace software started!")
+    try:
+        setup()
+        
+        barcodeThread = threading.Thread(target=getBarcodeData)
+        barcodeThread.start()
+        white_led_thread = LedThread(21, 15)
+        white_led_thread.start()
+        logging.info("Recyclace software started!")
+    except:
+        print("Unable to run Recyclace software!")
+        print("Error:", sys.exc_info()[0])
