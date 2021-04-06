c5932fde1baa8b4d4b319506097fbda2e1372ace
Ian Goh <ian@iangohy.com>
2021-04-06 08:41:00 +0800

    Fixed typos in recyclace_thread.py

--- recyclace_thread.py
+++ recyclace_thread.py
@@ -11,7 +11,7 @@ WASTE_PIN = 20
 
 BLINK_COUNT = 3
 
-LED_COUNTER = {"plastic": 0, "paper": 0, "can": 0, "waste": 0}}
+LED_COUNTER = {"plastic": 0, "paper": 0, "can": 0, "waste": 0}
 
 hid = { 4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g', 11: 'h', 12: 'i', 13: 'j', 14: 'k', 15: 'l', 16: 'm', 17: 'n', 18: 'o', 19: 'p', 20: 'q', 21: 'r', 22: 's', 23: 't', 24: 'u', 25: 'v', 26: 'w', 27: 'x', 28: 'y', 29: 'z', 30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' ', 45: '-', 46: '=', 47: '[', 48: ']', 49: '\\', 51: ';' , 52: '\'', 53: '~', 54: ',', 55: '.', 56: '/'  }
 
@@ -28,7 +28,7 @@ class LedThread(threading.Thread):
         global LED_COUNTER
         while True:
             # Check condition
-            if LED_COUNTER[self.category] != 0:
+            if LED_COUNTER[self.category] > 0:
                 logging.info(self.category + " HIGH")
                 GPIO.output(self.pin, GPIO.HIGH)
                 sleep(2)
@@ -105,10 +105,12 @@ def getBarcodeData():
                 return item[2]
         if not item:
             logging.info(f"Barcode {res} not found! Classifying as waste")
+            return "waste"
 
 def classifyWaste():
     while True:
         category = getBarcodeData()
+        print(category)
         LED_COUNTER[category] += BLINK_COUNT
 
 def setup():
