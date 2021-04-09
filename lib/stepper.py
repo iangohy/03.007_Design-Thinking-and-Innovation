9439657bb6ab893718ca469763bda704999b259b
Ian Goh <ian@iangohy.com>
2021-04-09 14:22:35 +0800

    Updated to write stepper percent

--- lib/stepper.py
+++ lib/stepper.py
@@ -2,12 +2,16 @@ from time import sleep
 import RPi.GPIO as GPIO
 
 class StepperMotor:
-    def __init__(self, enable_pin=17, dir_pin=27, pulse_pin=22, rps = 1, pulse_per_rev = 800):
+    def __init__(self, enable_pin=17, dir_pin=27, pulse_pin=22, rps = 1, pulse_per_rev = 800, percent_per_rev = 10):
         self.enable_pin = enable_pin
         self.dir_pin = dir_pin
         self.pulse_pin = pulse_pin
         self.rps = rps
         self.pulse_per_rev = pulse_per_rev
+        self.percent_per_rev = percent_per_rev
+        with open("stepper_loc.txt", "r") as location:
+            self.percent = location.read()
+            print(f"Stepper currently at {self.percent}%")
 
 
         GPIO.setmode(GPIO.BCM)
@@ -19,6 +23,7 @@ class StepperMotor:
         GPIO.output(enable_pin, GPIO.HIGH)
 
     def rotate(self, num_rev = 1):
+        # Set direction
         if num_rev >= 0:
             direction = "clockwise"
             GPIO.output(self.dir_pin, GPIO.HIGH)
@@ -38,6 +43,13 @@ class StepperMotor:
             GPIO.output(self.pulse_pin, GPIO.LOW)
             # print(".")
             sleep(sleep_len)
+
+        self.percent += num_rev * self.percent_per_rev
+        print(f"Stepper now at {self.percent}%")
+
+        with open("stepper_loc.txt", "w") as location:
+            location.write(self.percent)
+
 def main():
     nema23 = StepperMotor(rps = 1, pulse_per_rev = 1600)
     nema23.rotate(3)
--- recyclace_thread.py
+++ recyclace_thread.py
@@ -18,6 +18,7 @@ DIR_PIN = 27
 PUL_PIN = 22
 RPS = 1
 PULSE_PER_REV = 800
+PERECENT_PER_REV = 10
 
 BLINK_COUNT = 4
 
@@ -80,7 +81,7 @@ def moveMotorButtonPress():
     while True:
         if GPIO.input(BUTTON_PIN):
             logging.info("Button press!")
-            motor_1 = stepper.StepperMotor(enable_pin=ENA_PIN, dir_pin=DIR_PIN, pulse_pin=PUL_PIN, rps = RPS, pulse_per_rev = PULSE_PER_REV)
+            motor_1 = stepper.StepperMotor(enable_pin=ENA_PIN, dir_pin=DIR_PIN, pulse_pin=PUL_PIN, rps = RPS, pulse_per_rev = PULSE_PER_REV, percent_per_rev=PERCENT_PER_REV)
             motor_1.rotate(3)
             motor_1.rotate(-3)
             sleep(5)
